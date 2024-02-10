9bfeb526476b76f4e8e31bf6e59be6e73da162ea
RosVor <ros.vorokh@gmail.com>
2024-02-11 01:01:38 +0200

    app useEffect

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ContactForm from './ContactForm/ContactForm';
 import Filter from './Filter/Filter';
 import ContactList from './ContactList/ContactList';
+import { useDispatch } from 'react-redux';
+import { fetchContacts } from './redux/contactsSlice';
 import './ContactForm/ContactForm.css';
 import './ContactList/ContactList.css';
 import './Filter/Filter.css';
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
 
   return (
     <div className="contact-menu">
